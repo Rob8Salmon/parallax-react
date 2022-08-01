4fc7a5388b0b97a35d3bfd98accaddf0e13aaa51
Robert Salmon <robertsalmon@Roberts-MacBook-Pro.local>
2022-08-01 12:23:31 -0400

    initial commit

---
+++ src/components/TextBox.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TextBox = () => {
+  return (
+    <div className='text-box'>
+    <h3>First Class Ticket</h3>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam accusantium modi dignissimos minima quo eaque corrupti repellendus, perferendis molestias delectus iure voluptate officia quos id voluptates nam magnam numquam veritatis?,
+    </div>
+  )
+}
+
+export default TextBox
